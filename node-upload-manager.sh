866517b7d897b4a00b62bcb08466950ad23f4dc7
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-17 11:33:38 -0600

    added same flags as old filesync tool

--- node-upload-manager.sh
+++ node-upload-manager.sh
@@ -4,6 +4,10 @@ while true; do
     rsync -av \
     --exclude '*.tmp' \
     --remove-source-files \
+    --stats \
+    --partial-dir=.partial/ \
+    --timeout=120 \
+    --bwlimit=0 \
     /uploads \
     /remote
 
