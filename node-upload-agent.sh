4d11f3468fe0b4f5310f47ef851e03e3a1ace450
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-23 09:53:48 -0600

    fixed exclude. fixed /uploads to /uploads/ and /remote /remote/

--- node-upload-agent.sh
+++ node-upload-agent.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# the agent will rsync files between the following locations:
+# /uploads/plugin-name/... -> beehive:/home/node-id/plugin-name/...
+#
+# this allows us to maintain which node and plugin produced the data.
+
 while true; do
     rsync -av \
-    --exclude '*.tmp' \
+    --exclude '.tmp*' \
     --remove-source-files \
     --stats \
     --partial-dir=.partial/ \
     --timeout=120 \
     --bwlimit=0 \
-    /uploads \
-    /remote
+    /uploads/ \
+    /remote/
 
     sleep 60
 done
