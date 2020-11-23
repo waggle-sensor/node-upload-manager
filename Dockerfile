f7a16f77e979f2cc3ee0634d0bd701c0e5a4f0ae
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-23 15:57:05 -0600

    added config as env vars. create known_hosts from ca.pub. added BatchMode to prevent hanging.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM alpine
 RUN apk add openssh-client rsync
 COPY node-upload-agent.sh node-upload-agent.sh
-CMD ["sh", "node-upload-agent.sh"]
+ENTRYPOINT ["sh", "node-upload-agent.sh"]
--- node-upload-agent.sh
+++ node-upload-agent.sh
@@ -5,16 +5,36 @@
 #
 # this allows us to maintain which node and plugin produced the data.
 
+fatal() {
+    echo $*
+    exit 1
+}
+
+if [ -z "$WAGGLE_NODE_ID" ]; then
+    fatal "WAGGLE_NODE_ID is not defined"
+fi
+
+if [ -z "$WAGGLE_UPLOAD_HOST" ]; then
+    fatal "WAGGLE_UPLOAD_HOST is not defined"
+fi
+
+if ! echo "@cert-authority * $(cat /etc/waggle/ca.pub)" > /etc/ssh/ssh_known_hosts; then
+    fatal "could not read CA certificate or create known_hosts file"
+fi
+
+# safety sleep to prevent runaway rsync during restart loop
+sleep 10
+
 while true; do
-    rsync -av \
+    rsync -a \
+    -e "ssh -i /etc/waggle/ssh-key -o BatchMode=yes" \
     --exclude '.tmp*' \
     --remove-source-files \
-    --stats \
     --partial-dir=.partial/ \
     --timeout=120 \
     --bwlimit=0 \
-    /uploads/ \
-    /remote/
+    "/uploads" \
+    "node${WAGGLE_NODE_ID}@${WAGGLE_UPLOAD_HOST}:~/uploads/"
 
     sleep 60
 done
